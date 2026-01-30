7e39725fe6e505522f990755da85dbf6482c5cba
meghanamewati <meghanamewati@gmail.com>
2026-01-30 22:00:51 +0530

    Added all files

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics.Contracts;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public Rigidbody rb;
+    public float Move ;
+    public float SideMove ;
+    
+    void FixedUpdate()
+    {
+        rb.AddForce(0, 0, Move * Time.deltaTime );
+        Debug.Log("added force");
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            rb.AddForce  (SideMove * Time.deltaTime , 0, 0, ForceMode.VelocityChange);
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            rb.AddForce (- SideMove * Time.deltaTime , 0, 0, ForceMode.VelocityChange);
+        }
+    }
+}
