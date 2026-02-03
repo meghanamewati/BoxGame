bb773926cf47f39d99278d0e2de25cfffa44a706
meghanamewati <meghanamewati@gmail.com>
2026-02-03 19:28:29 +0530

    Level Started UI with Animation Done

---
+++ Assets/Scripts/LevelStartedPanel.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using TMPro;
+
+public class LevelStartedPanel : MonoBehaviour
+{   
+    public TMP_Text LevelStartedtext;
+    
+    void Start()
+    {   
+       // Text LevelStartedtext = FindObjectOfType<Text>();
+        int Activelevel =  SceneManager.GetActiveScene().buildIndex;
+        LevelStartedtext.text =   $"Level {Activelevel}";
+        Invoke("EndLevelStartPanel", 1.5f);
+    }
+
+    
+    void EndLevelStartPanel()
+    {
+        gameObject.SetActive(false);
+    }
+}
